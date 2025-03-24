a0fa39b2932374a706ac837434438ca92f8a6e4e
gabmin <rlarkqals@gmail.com>
2025-03-24 16:36:12 +0900

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <header>
+        <Link href={'/'}>index</Link>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
+}
