0c4673d2f857a47943cb81cf4abb73dce0f23b86
gabmin <rlarkqals@gmail.com>
2025-03-25 22:07:21 +0900

    [feat] vercel을 통한 배포 진행

--- src/lib/endpoints.ts
+++ src/lib/endpoints.ts
@@ -1,4 +1,8 @@
-const defaultUrl = 'http:localhost:12345';
+let defaultUrl = 'http:localhost:12345';
+
+if (process.env.NODE_ENV === 'production') {
+  defaultUrl = 'https://onebite-books-server-dun-beta.vercel.app/';
+}
 
 export const endpoints = {
   allBooks: `${defaultUrl}/book`,
