fd6942ed9bf3bfeac42f461e00e68282a81c1f64
gabmin <rlarkqals@gmail.com>
2025-03-25 21:14:42 +0900

    [feat] SSG 적용 방법 및 fallback 옵션 주석 추가

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,6 +1,7 @@
 import fetctTargetBooks from '@/lib/fetch-target-books';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import style from './[id].module.css';
+import { useRouter } from 'next/router';
 
 // css파일을 그냥 가져오게되면 클래스네임명이 중독되어 충돌되는 문제가 발생하여
 // 임의로 변환해주는 module 기능을 사용해서 가져와야함
@@ -13,21 +14,48 @@ export const getServerSideProps = async (
 
   const targetBook = await fetctTargetBooks(id);
 
+  // 데이터가 없을 경우 404페이지로 이동
+  if (!targetBook) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: {
-      ...targetBook,
+      targetBook,
     },
   };
 };
 
+// 동적 경로에서 SSG를 적용하기 위해서는 임의의 path를 지정해줘야한다.
+// getStaticPaths을 통해 지정해줄 수 있다.
+// fallback은 예상치 못한 경우가 발생할 경우 대비책을 지정할 수 있는 기능이다.
+// fallback: false 일 경우 404페이지가 노출됨
+// fallback: 'blocking'은 마치 SSR이 동작하는 것처럼 즉각적으로 생성해서 렌더링해준다. (.next 폴더에 생성된 파일이 저장됨)
+// fallback: true 일 경우 데이터 없이 컨포넌트만 렌더링하고 추후에 데이터를 응답받으면 그때 리렌더링한다.
+// 데이터를 응답 받기 전까지를 fallback 상태라고하며 router.isFallback를 통해 판별할 수 있다.
+// export const getStaticPaths = () => {
+//   return {
+//     paths: [
+//       { params: { id: '1' } },
+//       { params: { id: '2' } },
+//       { params: { id: '3' } },
+//     ],
+//     fallback: false,
+//   };
+// };
+
 export default function Page({
-  title,
-  subTitle,
-  author,
-  publisher,
-  description,
-  coverImgUrl,
+  targetBook,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+  if (router.isFallback) return '로딩중입니다...';
+  if (!targetBook) return '문제가 발생하였습니다. 다시 시도해주세요.';
+
+  const { title, subTitle, author, publisher, description, coverImgUrl } =
+    targetBook;
+
   return (
     <div className={style.container}>
       <div
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import fetchRandomBooks from '@/lib/fetch-random-books';
 // 자동으로 SSR 방식으로 동작하도록 변경
 // 컴포넌트보다 먼저 실행되어, 컴포넌트에 필요한 데이터를 불러오는 함수
 // 이 함수는 서버쪽에서만 실행되기 때문에 props로 리턴하지 않으면 접근할 수 없다. (window 객체 사용 불가능)
+// props type: InferGetServerSidePropsType<typeof getServerSideProps>
 export const getServerSideProps = async () => {
   const [allBooks, recommendBooks] = await Promise.all([
     fetchBooks(),
@@ -24,9 +25,19 @@ export const getServerSideProps = async () => {
   };
 };
 
+// SSG 정적 사이트 생성 방식
+// build time에 한번만 실행되기 떄문에 새고침을 해도 아무런 동작을 하지 않음
+// props type: InferGetStaticPropsType<typeof getStaticProps>
+// export const getStaticProps = () => {
+//   return {
+//     props: {},
+//   };
+// };
+
 // 서버 사이드에서 1번 Hydration 과정에서 1번 총 2번 실행된다. (window 객체 사용 불가능 - 서버 사이드일 경우)
 // useEffect를 사용하면 서버측에서 실행되지 않아서 window 객체를 사용할 수 있다.
 // InferGetServerSidePropsType - 자동으로 serverSideProps의 타입을 추론해준다.
+// 개발환경에서는 새로고침마다 실행됨 (preview 환경에서 정상적으로 동작함)
 export default function Home({
   allBooks,
   recommendBooks,
